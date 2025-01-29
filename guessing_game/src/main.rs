ceb1cd94e28ea8b7d241a209e11cf8c44726db66
Jack <jacktotten@hotmail.co.uk>
2025-01-29 20:21:28 +0000

    adding guessing game example.

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Hello! Guess the number.");
+    println!("Please input your guess:");
+
+    let mut guess = String::new(); // mut - mutable variable
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
