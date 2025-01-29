472b8811e0a544dfc031200ce2be12b69aac0534
Jack <jacktotten@hotmail.co.uk>
2025-01-29 21:04:20 +0000

    Adding functionality to check if the guess was higher or lower.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Hello! Guess the number.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number = {secret_number}");
+
     println!("Please input your guess:");
 
     let mut guess = String::new(); // mut - mutable variable
@@ -10,4 +17,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    };
 }
