96af8f2790d33c771a90299dfa6c3c39eff43506
Jack <jacktotten@hotmail.co.uk>
2025-02-01 22:24:39 +0000

    adding more control flow examples.

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let number = 3;
+    let number = 7;
 
     if number < 5 {
         println!("condition was true");
     } else {
         println!("condition was false");
     }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+    
 }
