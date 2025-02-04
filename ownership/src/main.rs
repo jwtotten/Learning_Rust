069839d5e36d45ea241427a886972f711838b2b3
Jack <jacktotten@hotmail.co.uk>
2025-02-04 18:00:35 +0000

    Adding example of refernces

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,6 +10,13 @@ fn main() {
                                     // but i32 is Copy, so it's okay to still
                                     // use x afterward
 
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1); // This uses a reference, so the ownership of the string
+                                            // still remains with s1, but calculate length can use it.
+
+    println!("The length of '{s1}' is {len}.");
+
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
 
@@ -21,3 +28,7 @@ fn takes_ownership(some_string: String) { // some_string comes into scope
 fn makes_copy(some_integer: i32) { // some_integer comes into scope
     println!("{some_integer}");
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
