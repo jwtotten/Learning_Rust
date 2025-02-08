629e98f1ef696524b916825cf605dc7b4466cb8a
Jack <jacktotten@hotmail.co.uk>
2025-02-08 14:25:40 +0000

    Adding example of mutable structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,8 +6,23 @@ fn main() {
         sign_in_count: 1,
     };
 
-    println!("User1: {}", user1.username);
+    println!("User1: {}", user1.username); // use dot notation to access fields
     println!("User1: {}", user1.email);
+
+    let mut user2 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    
+    println!("__________________________");
+
+    println!("User2: {}", user2.username); 
+    println!("User2: {}", user2.email);
+
+    user2.email = String::from("anotheremail@example.com"); // update field as user2 is mutable
+    println!("User2: {}", user2.email);
 }
 
 struct User {
