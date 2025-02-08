313c7356c866a43b16187b1bc9f9d44a9b485347
Jack <jacktotten@hotmail.co.uk>
2025-02-08 14:09:37 +0000

    updating use of string and string literal slices in find_first_word example

--- find_first_word/src/main.rs
+++ find_first_word/src/main.rs
@@ -3,20 +3,30 @@ fn main() {
 
     // `first_word` works on slices of `String`s, whether partial or whole
     let word = first_word(&my_string[0..6]);
+    println!("The first word is: {}", word);
     let word = first_word(&my_string[..]);
+    println!("The first word is: {}", word);
+    
     // `first_word` also works on references to `String`s, which are equivalent
     // to whole slices of `String`s
     let word = first_word(&my_string);
-
-    let my_string_literal = "hello world";
+    println!("The first word is: {}", word);
+    
+    let my_string_literal = "Hello world";
 
     // `first_word` works on slices of string literals, whether partial or whole
+    
     let word = first_word(&my_string_literal[0..6]);
+    println!("The first word is: {}", word);
+    
     let word = first_word(&my_string_literal[..]);
-
+    println!("The first word is: {}", word);
+    
     // Because string literals *are* string slices already,
     // this works too, without the slice syntax!
     let word = first_word(my_string_literal);
+    println!("The first word is: {}", word);
+    
 }
 
 fn first_word(s: &str) -> &str // s is a reference to a string slice as an input as it can take both a string slice and a string literal
