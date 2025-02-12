e5aa5b828a6f691a310ef3bf828aa15c5161d4f4
Jack <jacktotten@hotmail.co.uk>
2025-02-12 07:23:48 +0000

    Adding example of impl to a struct

--- area_of_rectangle/src/main.rs
+++ area_of_rectangle/src/main.rs
@@ -4,30 +4,20 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let rect1 = Rectangle{
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!{"____________________________________________________"};
-    println!("rect1 is {:#?}", rect1);
-    println!{"____________________________________________________"};
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-
-    println!{"____________________________________________________"};
-
-    let scale = 3;
-    let rect2 = Rectangle{
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!{&rect2};
-    println!("The area of the rectangle is {} square pixels.", area(&rect2));
-
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
