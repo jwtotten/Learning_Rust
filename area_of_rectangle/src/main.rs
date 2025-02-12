689bec81293d9ebe162e919e69c54f40cdd09b40
Jack <jacktotten@hotmail.co.uk>
2025-02-12 17:14:10 +0000

    Adding example of methods with additional parameters.

--- area_of_rectangle/src/main.rs
+++ area_of_rectangle/src/main.rs
@@ -8,6 +8,9 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other_rectangle: Rectangle) -> bool {
+        self.width > other_rectangle.width && self.height > other_rectangle.height
+    }
 }
 
 fn main() {
@@ -16,8 +19,22 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 10,
+        height: 60,
+    };
+
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    println!("Checking if rect 2 can fit within rect 1: {}", rect1.can_hold(rect2));
+    println!("Checking if rect 3 can fit within rect 2: {}", rect1.can_hold(rect3));
 }
