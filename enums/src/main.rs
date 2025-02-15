a4fc5be71f1f67cb473799d21c76dae2a62bd5d2
Jack <jacktotten@hotmail.co.uk>
2025-02-15 11:52:13 +0000

    Adding example of implementing enums with struct objects.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,9 +1,21 @@
+#[derive(Debug)]
 enum IpAddrKind {
     V4,
-    V6,
+    V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
 fn main() {
-    let four = IpAddrKind::V4;  
-    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("192.168.0.0"),
+    };
+
+    println!("Home IP address: {}", home.address);
+    println!("Home IP address kind: {:#?}", home.kind);
 }
