0964b311aee0db5b155c1805ec48ce8b9b5486fd
Jack <jacktotten@hotmail.co.uk>
2025-02-23 15:07:56 +0000

    Adding example of using the Options Enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,6 +4,7 @@ enum IpAddrKind {
     V6(String),
 }
 
+
 fn main() {
 
     let home = IpAddrKind::V4(String::from("192.168.0.0"));
@@ -11,4 +12,18 @@ fn main() {
 
     println!("Home IP address: {:#?}", home);
     println!("Loopback IP address: {:#?}", loopback);
+
+    println!("________________________________________________________");
+    println!("Example of Option enum");
+    println!("________________________________________________________");
+
+    // You dont need to include the Option enum in the prelude
+    // because it is used so often and it is included in the prelude
+    // by default.
+
+    let some_number= Some(5);
+    let some_character = Some('a');
+
+    let absent_number:Option<i32> = None;
+    
 }
